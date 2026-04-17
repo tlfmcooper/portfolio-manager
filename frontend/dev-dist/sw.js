0e3c8e400e24177e6d2f7608cff635e09c15b19f
Ali Kone <ali@alikone.dev>
2026-04-17 18:56:19 -0400

    fix:bug portfolio ytd calculation

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-0d7dea0d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.b99rc1crq8c"
+    "revision": "0.m58gcg0svrg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
